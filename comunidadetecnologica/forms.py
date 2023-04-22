f21c0f41bf0f63f0e7c40fe8d5d4bb057866670b
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-04-22 18:10:27 -0300

    Initial commit

---
+++ comunidadetecnologica/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class FormCriarConta(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField('Confirmar senha', validators=[
+        DataRequired(), EqualTo('senha')])
+    button_submit_criarconta = SubmitField('Criar conta')
+
+
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lembrar_dados = BooleanField('Lembrar-me')
+    button_submit_login = SubmitField('Fazer login')
