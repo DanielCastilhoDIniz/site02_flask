bb40e332654a8e76e4d15409a359147e2ce371bf
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-04-24 22:18:43 -0300

    formulário de edição de perfil

--- comunidadetecnologica/forms.py
+++ comunidadetecnologica/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from comunidadetecnologica.models import Usuario
-
+from flask_login import current_user
 
 class FormCriarConta(FlaskForm):
     username = StringField('Nome de Usuário', validators=[DataRequired()])
@@ -15,9 +15,8 @@ class FormCriarConta(FlaskForm):
     def validate_email(self, email):
         usuario = Usuario.query.filter_by(email=email.data).first()
         if usuario:
-            raise ValidationError('E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar.')
-
-        
+            raise ValidationError(
+                'E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar.')
 
 
 class FormLogin(FlaskForm):
@@ -32,3 +31,9 @@ class FormEiditarPerfil(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     button_submit_editarperfil = SubmitField('Confirmar edição')
 
+    def validate_email(self, email):
+        if current_user.email != email.data:
+            usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            raise ValidationError(
+                'Já existe um usuario com esse email. Cadastre-se com outro e-mail para continuar.')
