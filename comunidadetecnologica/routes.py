d449cb1d2f19242e267d195f31ec666d96b3aa55
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-04-26 22:28:15 -0300

    tags de curso cadastrados

--- comunidadetecnologica/routes.py
+++ comunidadetecnologica/routes.py
@@ -95,6 +95,15 @@ def salvar_imagem(imagem):
     imagem_reduzida.save(caminho_completo)
     return nome_arquivo
 
+def atualizar_cursos(form):
+    lista_cursos = []
+    for campo in form:
+        if 'curso_' in campo.name:
+            if campo.data:
+                lista_cursos.append(campo.label.text)
+    return ';'.join( lista_cursos)
+
+
 
 @app.route('/perfil/editar', methods=['GET', 'POST'])
 @login_required
@@ -106,6 +115,8 @@ def editar_perfil():
         if form.foto_perfil.data:
             nome_imagem = salvar_imagem(form.foto_perfil.data)
             current_user.foto_perfil = nome_imagem
+        
+        current_user.cursos = atualizar_cursos(form)
         database.session.commit()
         flash('Perfil atualizado com sucesso', 'alert-success')
         return redirect(url_for('perfil'))
