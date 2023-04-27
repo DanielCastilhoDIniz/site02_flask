a902ca3ac108cb894952aab27f724acda74ae319
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-04-27 20:57:52 -0300

    inserir table_name

--- comunidadetecnologica/__init__.py
+++ comunidadetecnologica/__init__.py
@@ -11,7 +11,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
 if os.getenv('DATABASE_URL'):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+else:   
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
