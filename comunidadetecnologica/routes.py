7cbb7cb6f1d49a4193311d55ab858b3b60ab8b17
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-04-27 14:29:12 -0300

    excluir posts, popup de confirmação de exclusão

--- comunidadetecnologica/routes.py
+++ comunidadetecnologica/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, abort
 from comunidadetecnologica import app, database
 from comunidadetecnologica import bcrypt
 from comunidadetecnologica.forms import FormLogin, FormCriarConta, FormEiditarPerfil, FormCriarPost
@@ -132,3 +132,36 @@ def editar_perfil():
     foto_perfil = url_for(
         'static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
     return render_template('editarperfil.html', foto_perfil=foto_perfil, form=form)
+
+
+@app.route('/post/<post_id>', methods=['GET','POST'])
+def exibir_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.autor:
+        form = FormCriarPost()
+        if request.method == 'GET':
+            form.titulo.data = post.titulo
+            form.corpo.data = post.corpo
+        elif form.validate_on_submit():
+            post.titulo = form.titulo.data
+            post.corpo = form.corpo.data
+            database.session.commit()
+            flash('Post atualizado com sucesso', 'alert-success')
+            return redirect(url_for('home'))
+    else:
+        form = None
+    return render_template('post.html', post=post, form=form)
+
+@app.route('/post/<post_id>/excluir', methods=['GET', 'POST', 'DELETE'])
+@login_required
+def excluir_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.autor:
+        database.session.delete(post)
+        database.session.commit()
+        flash('Post excluido com sucesso!', 'alert-danger')
+        return redirect(url_for('home'))
+    else:
+        abort(403)
+        
+
